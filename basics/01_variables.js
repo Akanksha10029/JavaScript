b3d7e6237c4736a21997aec7fd8ebb22196c0995
Akanksha10029 <akanksharani405@gmail.com>
2024-06-05 10:56:32 +0530

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountName = "Akanksha Rani"
+let accountEmail = "akanksha@google.com"
+let accountPassword =123456
+let accountState;
+
+console.log(accountName);
+console.log(accountEmail);
+console.log(accountPassword);
+
+console.table([accountName, accountEmail, accountPassword, accountState]);
+
+//prefer not to use "var" because of issue in block and functional scope

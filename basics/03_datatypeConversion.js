4c946812fdcb9133fcc14efeeed8b0544d35230c
Akanksha10029 <akanksharani405@gmail.com>
2024-06-05 23:12:32 +0530

    datatype conversions and operations

---
+++ basics/03_datatypeConversion.js
@@ -0,0 +1,73 @@
+// let score = "40"
+
+// console.log(score);
+// console.log(typeof score);
+
+// let scoreInNumber = Number(score)
+
+// console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// Conversion from one datatype to another datatype (here "Number")
+//"33" -> 33 
+// "33abc" -> NaN
+// null -> 0
+//undefined -> NaN
+// true -> 1, False -> 0
+
+
+// let isLoggedIn = 1
+// console.log(isLoggedIn)
+// console.log(typeof isLoggedIn);
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// 1 -> true, 0 -> false
+// ""(empty string) -> false
+// "Akanksha Rani" -> true
+
+// let anyNumber = 45  // try null,"", undefined
+// console.log(anyNumber);
+// console.log(typeof anyNumber);
+
+// let stringNumber = String(anyNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//+++++++++++++++++++ Operations +++++++++++++++++++
+
+// console.log(2+3);  // 5
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(2 + 4 + "3"); // 63
+
+// console.log(+true); //1 
+// console.log(+""); //0
+
+
+
+// Increment(++) operator ->
+
+// let x=4
+// let y=x++
+// console.log(`x:${x}, y:${y}`); // x:5, y:4
+
+// let a=7
+// let b=++a
+// console.log(`a is ${a} and b is ${b}`); // a is 8 and b is 8
+
+
+
+// decrement (--) operator ->
+
+let x1 = 5n; 
+let y1 = x1--;
+console.log(`x:${x1}, y:${y1}`); // x:4, y:5
+
+let x2 = 5n
+let y2 = --x2
+console.log(`x:${x2}, y:${y2}`); // x:4, y:4

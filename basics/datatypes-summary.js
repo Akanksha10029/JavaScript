ebf8deef661af2c8d701cf33026200039af279dd
Akanksha10029 <akanksharani405@gmail.com>
2024-06-06 01:47:09 +0530

    primitive type

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// On the bais of how data is stored in memory and how it can be accessed, for this we have 2 categorizations --> 
+//1. Primitive (call by value type)  -> 7 categories --> String,Number,Boolean,null,undefined,Symbol, BigInt
+
+let anyString = "Akanksha Rani"
+let anyNumber = 29
+let anyBoolean = true
+let anyNullvalue = null
+let anyUndefined = undefined
+
+//2. Non- Primitive (Reference type) --> Array, Objects, functions

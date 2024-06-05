7a5ba03add4099695819b3b9da5bfc41e346aff7
Akanksha10029 <akanksharani405@gmail.com>
2024-06-05 18:49:57 +0530

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hallo")  we are using nodeJS not browser
+
+let name ="Akanksha Rani"
+let age = 20
+let isLoggedIn =true
+
+// Datatypes: number => 2^53
+// bigint 
+// string => " "
+// boolean =True/False
+// null => stand alone value
+// undefined => value is not defined
+// symbol => to find uniqueness
+// object
+
+console.log(typeof(name)); // => string
+console.table([typeof(isLoggedIn)]) // => boolean
+
+console.log(typeof(null)); // => object
+console.log(typeof(undefined)); // => undefined

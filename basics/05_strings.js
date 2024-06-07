fd2f5431ad31516468affc0cfd1e1a161015bc9a
Akanksha10029 <akanksharani405@gmail.com>
2024-06-07 23:09:34 +0530

    Strings and its various methods use

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -25,7 +25,25 @@ const newName = myName.substring(0,4) //in substring, we can't pass negative par
 console.log(newName);
 
 // Doubt 
-const anotherNameString =myName.slice(-8,0)
+const anotherNameString =myName.slice(-7,15)
 console.log(anotherNameString);
+
+let newString = "   Prashant "
+console.log(newString);
+console.log(newString.trim()); //trim() method removes whitespace from both ends of a string and returns a new String, without modifying the original string
+
+
+console.log(newString.trimStart()); //Trim whitespaces from the start.
+console.log(newString.trimEnd()); //Trim whitespaces from the end.
+
+
+const url = "https://Akanksha%20Rani.com/Akanksha%20Javascript"
+
+console.log(url.replace("%20", "_")); //here only first occurance of %20 get removed, not all the %20 present in the string.
+console.log(url.replace(/%20/g, "_")); //Here to replace global (g) flag is used to replace all the instances of %20 with"_".
+
+console.log(url.includes("beauty")); // here it checks that "beauty" includes in the string or not.
+
+console.log(myName.split("-"));
 
 

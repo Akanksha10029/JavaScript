6a9df67b0d9c810c9c033892e679d922ee6b58e1
Akanksha10029 <akanksharani405@gmail.com>
2024-06-09 22:50:12 +0530

    global and local scope,  nested functions and calling of functions

--- Arrays_0bjects_functions/06_scopes.js
+++ Arrays_0bjects_functions/06_scopes.js
@@ -7,3 +7,33 @@ if(true){
     console.log(`The inner user name is ${name} and the inner price is ${price}`);
 }
 console.log(`the outer name is ${name} and the outer price is ${price}`);
+
+
+//nested functions   
+function one(){
+    let name = "prashant kumar"
+    function two(){
+        let name = "Akanksha rani"
+        console.log(name);
+    }
+    two()
+    console.log(name);
+}
+one()
+//output: Akanksha rani
+//        prashant kumar
+
+// +++++++++++++++++ interesting ++++++++++++++
+
+console.log(addOne(4))
+function addOne(num){
+    return num+1
+}
+
+
+// console.log(addTwo(4)); //ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function (num){
+    return num+2
+}
+console.log(addTwo(4));
+

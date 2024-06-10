d3c095da36b3cee765d928bdddb7d590614d3f61
Akanksha10029 <akanksharani405@gmail.com>
2024-06-11 01:47:53 +0530

    while and do while loop concepts

---
+++ Loops_or_iterations/02_while_do_while.js
@@ -0,0 +1,12 @@
+let myHeroes = ["superman","iron man","spiderman"]
+let arr = 0
+while (arr < myHeroes.length) {
+    console.log(`It's time for ${myHeroes[arr]}`);
+    arr++
+}
+
+let score = 17
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);

1948ffe7b55a8a9521ee10614d80aef84d3affd6
Akanksha10029 <akanksharani405@gmail.com>
2024-06-10 23:31:48 +0530

    switch cases

---
+++ Control_flow/02_switch.js
@@ -0,0 +1,25 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+
+    default:
+        console.log("Enter valid month number");
+        break;
+}

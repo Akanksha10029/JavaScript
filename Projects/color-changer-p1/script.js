a424b3afe1ccc3f6c643bd3216fdaa18824bf470
Akanksha10029 <akanksharani405@gmail.com>
2024-06-16 20:57:55 +0530

    backgroud color changer project

---
+++ Projects/color-changer-p1/script.js
@@ -0,0 +1,31 @@
+const bgBox = document.querySelector(".box")
+const btn = document.querySelectorAll(".btn")
+
+btn.forEach(e => {
+
+    e.addEventListener("click", ev => {
+        if (ev.target.id === "gray") {
+            bgBox.style.backgroundColor = ev.target.id
+        }
+        else if (ev.target.id === "yellow") {
+            bgBox.style.backgroundColor = ev.target.id
+        }
+        else if (ev.target.id === "purple") {
+            bgBox.style.backgroundColor = ev.target.id
+        }
+        else if (ev.target.id === "pink") {
+            bgBox.style.backgroundColor = ev.target.id
+        }
+    })
+});
+
+const box = document.querySelector(".box")
+const body = document.querySelector("body")
+
+body.addEventListener("click", () => {
+    box.style.backgroundColor = "orange";
+});
+
+box.addEventListener("click", ev => {
+    ev.stopPropagation();
+});

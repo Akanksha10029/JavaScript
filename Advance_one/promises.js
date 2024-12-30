0df081859d121e44c09ea746c094ea3da59a49c6
Akanksha10029 <akanksharani405@gmail.com>
2024-12-30 22:37:26 +0530

    async func, using .then() and .catch(), async/await

--- Advance_one/promises.js
+++ Advance_one/promises.js
@@ -58,3 +58,53 @@ promiseFour.then((user)=>{
 }).finally(()=>{ //it will run in any case
     console.log("Promise 4 completed either by giving success or failure");
 })
+
+//We can use async/await with promises just like we did by using then(), catch() and finally()
+const promiseFive = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            resolve({userName: "John", age: 25, email: ""})
+            console.log("Async operation 5 completed");
+        }
+        else{
+            reject("Error: Something went wrong");
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    }catch (error) {
+        console.log(error);  
+    }
+}
+// Call the async function
+consumePromiseFive()
+
+//fetch API returns a promise
+async function getAllUsers() {
+    try {
+        const users = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await users.json();
+        console.log(data);
+        
+    } catch (error) {
+        console.log("Error: ", error); 
+    }
+}
+getAllUsers()
+
+//fetch API by using then() and catch()
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    log(data);
+})
+.catch((error)=>{
+    console.log("Error: ", error);
+})
